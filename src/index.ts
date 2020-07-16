66238ea0bf8fd5c95df6e67d84d2f75c07089273
jonataspinto <jonatas.pferreira@b2wdigital.com>
2020-07-16 01:37:58 -0300

    chore: add lib cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import {
   playerRoutes,
   serieRoutes,
@@ -9,6 +10,7 @@ config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(playerRoutes);
 app.use(serieRoutes);
