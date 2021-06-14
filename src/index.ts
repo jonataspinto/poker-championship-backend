d4371cc4bf787c50e7d389fd4f348f4cd052391a
Jonatas <jonatastdb_jp@hotmail.com>
2021-06-14 16:07:31 -0300

    chore: remove module alias config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import "module-alias/register";
+// import "module-alias/register";
 import express from "express";
 import { userRouter } from "./routes/UserRouter";
 import { journeyRouter } from "./routes/JourneyRouter";
