4c2d053bfebb28ef8887d608fe09f5d5fdc8f31d
jonataspinto <jonatastdb_jp@hotmail.com>
2023-11-15 16:18:58 -0300

    feat: implements database interface and mock

---
+++ src/__mock__/database.ts
@@ -0,0 +1,47 @@
+class DATABASE_MOCK implements IDBProvider {
+  private data: Array<any> = [];
+
+  async save<T>(payload: T): Promise<T> {
+    return new Promise((resolve) => {
+      const value = {
+        ...payload,
+        id: this.getUUID(),
+      };
+      this.data.push(value);
+      resolve(value);
+    });
+  }
+
+  async getAll<T>(): Promise<Array<T>> {
+    return new Promise((resolve) => {
+      resolve(this.data as Array<T>);
+    });
+  }
+
+  async update<T>(id: string, payload: T): Promise<T> {
+    return new Promise((resolve) => {
+      const updatedValue = ({ id, ...payload });
+      this.data = this.data.map((row) => (row?.id === id ? updatedValue : row));
+      resolve(updatedValue);
+    });
+  }
+
+  async getById<T>(id: string): Promise<T> {
+    return new Promise((resolve) => {
+      resolve(this.data.find((row) => row.id === id) as T);
+    });
+  }
+
+  async delete(id: string): Promise<string> {
+    return new Promise((resolve) => {
+      this.data = this.data.filter((row) => row.id !== id);
+      resolve(id);
+    });
+  }
+
+  getUUID() {
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+  }
+}
+
+export default new DATABASE_MOCK();
---
+++ src/database/IDatabase.d.ts
@@ -0,0 +1,7 @@
+interface IDBProvider {
+  save: <T>(data: T) => Promise<T>
+  update: <T>(id: string, payload: T) => Promise<T>
+  delete: (id: string) => Promise<string>
+  getAll: <T>() => Promise<Array<T>>
+  getById: <T>(id: string) => Promise<T>
+}
