a38f92e846a3356f28726a2e756808da34f13b07
jonataspinto <jonatastdb_jp@hotmail.com>
2023-11-16 00:40:32 -0300

    feat: using routes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
-// import "module-alias/register";
 import express from "express";
-import { userRouter } from "./routes/UserRouter";
-import { journeyRouter } from "./routes/JourneyRouter";
-import { seasonRouter } from "./routes/SeasonRouter";
-import { cupRouter } from "./routes/CupRouter";
 import { AppConfig } from "./config";
+import { Routes } from "./routes";
 
 const app = AppConfig(express());
 
-app.use(userRouter);
-app.use(journeyRouter);
-app.use(seasonRouter);
-app.use(cupRouter);
+Routes.use(app);
