6facad0d024c93b3972675bf1f4bc43322fad91b
jonataspinto <jonatastdb_jp@hotmail.com>
2023-11-15 23:50:40 -0300

    refactor(interfaces/Player): create DTO interface

--- src/interfaces/Player.d.ts
+++ src/interfaces/Player.d.ts
@@ -1,11 +1,14 @@
 interface IPlayer {
   name: string;
   email: string;
-  id: string;
   displayName?: string;
   dateBirth?: Date | string;
   photoURL?: string;
   address?: IAddress;
   points?: number;
   isAdmin?: boolean
+}
+
+interface IPlayerDTO extends IPlayer {
+  id: string
 }
