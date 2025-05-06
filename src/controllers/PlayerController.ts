e4eb6c3bd6b659d56f9b122bf8e6ee7eb65a8c2f
jonataspinto <jonatastdb_jp@hotmail.com>
2025-05-06 00:00:26 -0300

    refactor: enhance PlayerController to use PlayerMapper and sanitizeObject for data handling

--- src/controllers/PlayerController.ts
+++ src/controllers/PlayerController.ts
@@ -1,19 +1,24 @@
 import { Request, Response } from "express";
 import { Controller } from "./Controller";
 import PlayersRepository from "../repositories/Players/PlayersRepository";
+import { PlayerMapper } from "@/mappers/players";
+import { sanitizeObject } from "@/utils";
 
 class PlayerController implements Controller {
   async index(request: Request, response: Response) {
     const players = await PlayersRepository.findAll();
 
-    response.json(players);
+    const data = players.map(PlayerMapper.toDomain);
+
+    response.json(data);
   }
 
   async store(request: Request, response: Response) {
-    const payload = request.body as IPlayer;
-    const player = await PlayersRepository.create(payload);
+    const payload = sanitizeObject(
+      PlayerMapper.toPersistence(request.body)
+    ) as IPlayer;
 
-    if (!payload.name || payload.email) {
+    if (!payload.name || !payload.email) {
       response.status(400).send({ error: "Name and Email is required" });
       return;
     }
@@ -24,6 +29,8 @@ class PlayerController implements Controller {
       response.status(400).send({ error: "This email is already in use" });
       return;
     }
+
+    const player = await PlayersRepository.create(payload);
 
     response.status(201).json(player);
   }
@@ -44,33 +51,22 @@ class PlayerController implements Controller {
       return;
     }
 
-    response.json(player);
+    response.json(PlayerMapper.toDomain(player));
   }
 
   async update(request: Request, response: Response) {
     const { id } = request.params;
-    const payload = request.body;
 
-    const playerExists = await PlayersRepository.findById(payload.id);
+    const playerExists = await PlayersRepository.findById(id);
 
     if (!playerExists) {
       response.status(404).send({ error: "Player not found" });
       return;
     }
 
-    if (!payload.name || payload.email) {
-      response.status(400).send({ error: "Name and Email is required" });
-      return;
-    }
-
-    const playerExistsByEmail = await PlayersRepository.findByEmail(
-      payload.email
-    );
-
-    if (playerExistsByEmail && playerExistsByEmail.id !== id) {
-      response.status(400).send({ error: "This email is already in use" });
-      return;
-    }
+    const payload = sanitizeObject(
+      PlayerMapper.toPersistence(request.body)
+    ) as IPlayer;
 
     const player = await PlayersRepository.update(id, payload);
 
