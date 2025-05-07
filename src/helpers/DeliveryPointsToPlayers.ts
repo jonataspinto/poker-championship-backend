188593c6c094ddd5c3357cae8b04ccd34c41d80e
jonataspinto <jonatastdb_jp@hotmail.com>
2025-05-07 14:17:17 -0300

    refactor: improve variable naming and streamline player data retrieval in DeliveryPointsToPlayers

--- src/helpers/DeliveryPointsToPlayers.ts
+++ src/helpers/DeliveryPointsToPlayers.ts
@@ -27,19 +27,37 @@ export class DeliveryPointsToPlayers {
   }
 
   async deliveryPodium(): Promise<void> {
-    const podium = Object.entries(this.podium);
+    const podium = Object.entries(this.podium) as [
+      keyof IPlayerPodium,
+      string
+    ][];
 
-    podium.forEach(async (podiumPosition: Array<string>) => {
+    podium.forEach(async (podiumPosition) => {
       const key = podiumPosition[0];
       const value = podiumPosition[1];
-      if (value) {
-        const userData = await this.dbAdapter.getById(value);
+      if (key && value) {
+        const player = await this.dbAdapter.getById(value);
 
-        const { id, points = 0, ...rest } = userData;
+        const {
+          id,
+          points = 0,
+          podiums = {
+            first: 0,
+            second: 0,
+            third: 0,
+            fourth: 0,
+            fifth: 0
+          },
+          ...rest
+        } = player;
 
         await this.dbAdapter.update(id as string, {
           ...rest,
-          points: points + parseInt(Points[key as any], 10)
+          points: points + parseInt(Points[key as any], 10),
+          podiums: {
+            ...player.podiums,
+            [key]: (podiums?.[key] || 0) + 1
+          }
         });
       }
     });
@@ -47,9 +65,9 @@ export class DeliveryPointsToPlayers {
 
   async deliveryBiggestEliminator(): Promise<void> {
     if (this.biggestEliminator) {
-      const userData = await this.dbAdapter.getById(this.biggestEliminator);
+      const player = await this.dbAdapter.getById(this.biggestEliminator);
 
-      const { id, points = 0, ...rest } = userData;
+      const { id, points = 0, ...rest } = player;
 
       await this.dbAdapter.update(id as string, {
         ...rest,
@@ -60,9 +78,9 @@ export class DeliveryPointsToPlayers {
 
   async deliveryBestHandPoints(): Promise<void> {
     if (this.bestHand) {
-      const userData = await this.dbAdapter.getById(this.bestHand);
+      const player = await this.dbAdapter.getById(this.bestHand);
 
-      const { id, points = 0, ...rest } = userData;
+      const { id, points = 0, ...rest } = player;
 
       await this.dbAdapter.update(id as string, {
         ...rest,
